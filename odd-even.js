07fe0893e76bcbef7f20cdd8618cf66eebec8b08
Courage OmniBarry <135624085+AndreOmniBarry@users.noreply.github.com>
2023-06-27 18:00:13 +0100

    Add files via upload

---
+++ odd-even.js
@@ -0,0 +1,11 @@
+// check even or odd numbers
+n = parseInt(prompt("Enter a number: "));
+
+//if the number is divided by 2 without remainder, it is even 
+if(n % 2 == 0) {
+    console.log("This is an even number");
+}
+// else its an odd number
+else{
+    console.log("This is an odd number")
+}
